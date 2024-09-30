83d06c61cd340f4ba744c1fecdf39a18c5472ae8
btx <bernardo.tolosa@gcash.com>
2024-09-30 10:24:33 +0800

    initial commit

---
+++ Sources/HelloPackage/HelloPackage.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct HelloPackage {
+    public init() {}
+    
+    public func HelloPackage() {
+        print("Hello, World!")
+    }
+}
